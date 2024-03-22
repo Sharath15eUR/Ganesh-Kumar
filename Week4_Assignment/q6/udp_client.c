49b891148468a1a0bcffe4ac7c50f8c9f76e5894
Ganesh kumar <137510370+Ganeshkumar-06@users.noreply.github.com>
2024-03-22 16:44:31 +0530

    Create udp_client.c

---
+++ Week4_Assignment/q6/udp_client.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    socklen_t addr_size = sizeof(server_addr);
+    char buffer[BUFFER_SIZE];
+
+    // Create socket
+    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    // Send data to server
+    sendto(client_socket, "Hello from client!", 18, 0, (struct sockaddr *)&server_addr, addr_size);
+    printf("Message sent to server\n");
+
+    // Receive response from server
+    recvfrom(client_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &addr_size);
+    printf("Response from server: %s\n", buffer);
+
+    // Close socket
+    close(client_socket);
+
+    return 0;
+}
