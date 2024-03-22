49fcc8b31a1ac366c016e51e4ca18abe89c290f7
Ganesh kumar <137510370+Ganeshkumar-06@users.noreply.github.com>
2024-03-22 16:42:09 +0530

    Create client.c

---
+++ Week4_Assignment/q5/client.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // Create socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+    // Connect to server
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get user input
+    printf("Enter a string: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+
+    // Send string to server
+    send(client_socket, buffer, strlen(buffer), 0);
+    printf("Message sent to server\n");
+
+    // Receive response from server
+    recv(client_socket, buffer, BUFFER_SIZE, 0);
+    printf("Response: %s\n", buffer);
+
+    // Close socket
+    close(client_socket);
+
+    return 0;
+}
